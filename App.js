e2599b03e89db2b55a811eeb2910fbc4dd50e2ba
root <daniel.farnos.e@gmail.com>
2018-09-30 21:45:45 +0200

    test navigation

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { PathNotAutenticated } from './Components/notAuth/PathNotAuthenticated';
+
 
 export default class App extends React.Component {
   constructor() {
@@ -10,7 +12,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <PathNotAutenticated />
+
       </View>
     );
   }
@@ -20,7 +23,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
