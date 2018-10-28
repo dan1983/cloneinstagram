7afcae28ca947e66729d1e976a0fe0dee5cc9a72
root <daniel.farnos.e@gmail.com>
2018-10-28 15:12:15 +0100

    firebase

--- Store/Services/Firebase.js
+++ Store/Services/Firebase.js
@@ -4,12 +4,7 @@ import * as firebase from 'firebase';
 
 
 const config = {
-  apiKey: 'AIzaSyC5Wyk-m8ARn9c8kq_rcnEBc5fC5YbfVVo',
-  authDomain: 'instaclone-c83e7.firebaseapp.com',
-  databaseURL: 'https://instaclone-c83e7.firebaseio.com',
-  projectId: 'instaclone-c83e7',
-  storageBucket: 'instaclone-c83e7.appspot.com',
-  messagingSenderId: '571878434444',
+
 };
 
 
